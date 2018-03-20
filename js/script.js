1914ad686802c17f52f968d7952b28e1f7405607
Hamza Siddiqui <hamza.siddiqui.work@gmail.com>
2018-03-20 12:33:24 +0500

    PEHLA PAGE

--- js/script.js
+++ js/script.js
@@ -5,13 +5,13 @@ $(function(){
 			var that = $(this);
 			var s = parseInt($(this).attr("data-rating"));
 			for(i=0;i<5;i++){
-				var clss="star";
+				var clss="fa fa-star";
 				if(s>0){
 					clss+=" on";
 					s--;
 				}
 
-				that.append("<div class='"+clss+"'></div>");
+				that.append("<span class='"+clss+"'></span>");
 			}
 		});
 	}
@@ -31,7 +31,8 @@ $(function(){
 			data = $.parseJSON(data);
 			for(var r in data){
 				$h = data[r];
-				$("#featuredhuts").append('<div class="hut"><img src="'+$h['url']+'" alt="'+$h['name']+'" class="profile"><div class="container"><div class="full"><h3>'+$h['name']+'</h3></div></div><div class="container"><div class="half"><div class="rating" data-rating="'+$h['rating']+'"></div></div><div class="half location"><span>'+($h['area']==null ? '':$h['area']+' | ')+'</span> <span>'+Cities[$h['city']]+'</span></div></div><div class="container"> <p class="desc">'+$h['description']+'</p> </div> <a href="#0"  data-hut-id="'+$h['id']+'" class="card_button">Book It</a> </div>');
+				$area= $h["area"]==null ? "" : $h["area"]+" | ";
+				$("#featuredhuts .row").append('<div class="card" style="width:280px;"><img class="card-img-top" src="'+$h["url"]+'" alt="'+$h["name"]+'"><div class="card-body"><h5 class="card-title">'+$h["name"]+'<br><small class="text-muted">'+$area+Cities[$h["city"]]+'</small></h5><div class="rating" data-rating="'+$h["rating"]+'"></div><p class="card-text">'+$h["description"]+'</p><a href="#0" hut-id="'+$h["id"]+'" class="btn btn-primary">Book It</a></div></div>');
 
 			}
 			rate();
